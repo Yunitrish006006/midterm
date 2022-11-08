46e152ef4a26f53bb3ea52572127da20025be479
Yunitrish <yunthomas006@gmail.com>
2022-11-09 00:40:03 +0800

    結果

--- s0954005_dtree.py
+++ s0954005_dtree.py
@@ -10,20 +10,27 @@ clf = tree.DecisionTreeClassifier(random_state=0)
 data = pd.read_csv("taipei.csv")
 #replace
 data.fillna({'受傷程度': 0},inplace=True)
-# data['天候'].replace([1,2,3,4,5,6,7,8], [0,0,1,1,2,2,3,3], inplace=True)
-# data['光線'].replace([1,2,3,4], [0,0,1,1], inplace=True)
+data.fillna({'飲酒情形': 0},inplace=True)
+
+#濾除酒駕、無法測量的
+data = data[data['受傷程度']!=1]
+data = data[data['飲酒情形']<10]
+#濾除天氣過爛的
+# data = data[data['天候']>5]
+#濾除光線過暗的
+data = data[data['光線']>1]
 #導入資料
-X = data[['光線','天候']]
+# X = data[['光線']]
+X = data[['天候']]
+# X = data[['飲酒情形']]
 y = data[["受傷程度"]]
 #訓練
-X_train, X_test, y_Train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+X_train, X_test, y_Train, y_test = train_test_split(X, y, test_size=0.45, random_state=2)
 #trainer
 dtree =tree.DecisionTreeClassifier()
 clf = dtree.fit(X_train, y_Train)
 print("準確率 :", dtree.score(X_test, y_test))
 
-# preds= dtree.predict_proba(X=X_test)
-# print(pd.crosstab(preds[:,0], columns=[X["willWait"],X_test["pat"]]))
 plt.figure()
-plot_tree(clf, filled=True,fontsize=5)
+plot_tree(clf, filled=True,fontsize=7)
 plt.show()
